cb8df131c389b6d72c81eea5f3e8c80c66e8e960
Matti Määttänen <matti.maatt@gmail.com>
2021-03-06 09:07:31 +0200

    added source for default dev env variables

--- compose-dev.sh
+++ compose-dev.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/bash
 
+# set exit immediately after errors
 set -e
+
+# print commands and arguments
 set -x
+
+# all assignments arguments are palced in the environment
+set -k
+
+# mark variables which are modified or created for export
+set -a
 
 # This script will use docker-compose-dev.yml
 # and pass command line arguments
+
+# lets source the .env-dev file as basis
+source .env-dev
 
 docker-compose -f docker-compose-dev.yml $@

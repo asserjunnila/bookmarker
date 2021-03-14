ee357ae5f23b23700adb9b1356ec66ecbb4511a9
Asser Junnila <asser.junnila@gmail.com>
2021-03-14 22:34:58 +0200

    testing out eslint and prepping for a massive merge

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,36 +3,32 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import bookRoutes from './routes/routes';
 
-const app = express()
-app.use(cors())
-
-//const dotenv = require('dotenv');
-//dotenv.config({ path: './config/.env' });
+const app = express();
+app.use(cors());
 
 const port = process.env.REACT_APP_BACKPORT || 8080;
-const DB_URL = process.env.MONGO_URL || "mongodb://localhost:27018/books";
+const DB_URL = process.env.MONGO_URL || 'mongodb://localhost:27018/books';
 
-console.log(DB_URL);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect(DB_URL, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 }).then(() => {
-    console.log("Successfully connected to the database", DB_URL);
-}).catch(err => {
-    console.log(`Could not connect to the database ${DB_URL}. Exiting now...`, err);
-    process.exit();
+  console.log('Successfully connected to the database', DB_URL); // eslint-disable-line no-console
+}).catch((err) => {
+  console.log(`Could not connect to the database ${DB_URL}. Exiting now...`, err); // eslint-disable-line no-console
+  process.exit();
 });
 
 app.get('/', (req, res) => {
-    res.json({ "message": "Welcome to the book database backend." });
+  res.json({ message: 'Welcome to the book database backend' });
 });
 app.use(bookRoutes);
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
-})
+  console.log(`Server listening at http://localhost:${port}`); // eslint-disable-line no-console
+});

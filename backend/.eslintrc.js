a636c72290dc49bf864eab248090f1c39f47a1ee
Matti Määttänen <matti.maatt@gmail.com>
2021-03-14 20:50:48 +0200

    configurations for ts-node and eslinter

---
+++ backend/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: [ './tsconfig.json' ]
+  }
+};

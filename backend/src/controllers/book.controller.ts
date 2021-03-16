68f5af0ce5c056020d8252cca8198465ded75541
Matti Määttänen <matti.maatt@gmail.com>
2021-03-16 06:02:06 +0200

    20% of lint errors left \o

--- backend/src/controllers/book.controller.ts
+++ backend/src/controllers/book.controller.ts
@@ -1,123 +1,121 @@
-// const Book = require('../models/book.model');
 import { Request, Response } from 'express';
 import Book from '../models/book.model';
 
-
-export function create(req: Request, res: Response) {
-    // Validate request
-    // TODO: rest of the fields
-    if (!req.body.bookName || !req.body.bookAuthor || !req.body.bookMark
-        || !req.body.bookMarkDate || !req.body.bookPages || !req.body.readStartDate) {
-        return res.status(400).send({
-            message: "missing info"
-        });
-    }
-
-    const book = new Book({
-        bookName: req.body.bookName,
-        bookAuthor: req.body.bookAuthor,
-        bookImg: req.body.bookImg,
-        bookMark: req.body.bookMark,
-        bookMarkDate: req.body.bookMarkDate,
-        bookPages: req.body.bookPages,
-        readStartDate: req.body.readStartDate,
+export function create(req: Request, res: Response): void {
+  // Validate request
+  if (!req.body.bookName || !req.body.bookAuthor || !req.body.bookMark
+    || !req.body.bookMarkDate || !req.body.bookPages || !req.body.readStartDate) {
+    res.status(400).send({
+      message: 'missing info',
     });
+  }
 
-    book.save()
-        .then(data => {
-            res.send(data);
-        }).catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the book. Try again"
-            });
-        });
-};
+  const book = new Book({
+    bookName: req.body.bookName,
+    bookAuthor: req.body.bookAuthor,
+    bookImg: req.body.bookImg,
+    bookMark: req.body.bookMark,
+    bookMarkDate: req.body.bookMarkDate,
+    bookPages: req.body.bookPages,
+    readStartDate: req.body.readStartDate,
+  });
 
-export function findAll(req: Request, res: Response) {
-    Book.find()
-        .then(books => {
-            res.send({ "books": books });
-        }).catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving books."
-            });
-        });
-};
+  book.save()
+    .then((data) => {
+      res.send(data);
+    }).catch((err) => {
+      res.status(500).send({
+        message: err.message
+      });
+    });
+}
 
-export function findOne(req: Request, res: Response) {
-    Book.findById(req.params.bookId)
-        .then(book => {
-            if (!book) {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            res.send(book);
-        }).catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            return res.status(500).send({
-                message: "Error retrieving book with id " + req.params.bookId
-            });
-        });
-};
+export function findAll(req: Request, res: Response): void {
+  Book.find()
+    .then((foundBooks) => {
+      res.send({ books: foundBooks });
+    }).catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+}
 
-export function update(req: Request, res: Response) {
-    // Validate Request
-    if (!req.body.bookName || !req.body.bookAuthor || !req.body.bookPages || !req.body.bookMarkDate || !req.body.bookPages || !req.body.readStartDate) {
-        return res.status(400).send({
-            message: `needed book info can not be empty`
+export function findOne(req: Request, res: Response): void {
+  Book.findById(req.params.bookId)
+    .then((book) => {
+      if (!book) {
+        res.status(404).send({
+          message: `book not found with id ${req.params.bookId}`,
         });
-    }
+      }
+      res.send(book);
+    }).catch((err) => {
+      if (err.kind === 'ObjectId') {
+        res.status(404).send({
+          message: `book not found with id ${req.params.bookId}`,
+        });
+      }
+      res.status(500).send({
+        message: `Error retrieving book with id ${req.params.bookId}`,
+      });
+    });
+}
 
-    Book.findByIdAndUpdate(req.params.bookId, {
-        bookName: req.body.bookName,
-        bookAuthor: req.body.bookAuthor,
-        bookImg: req.body.bookImg,
-        bookMark: req.body.bookMark,
-        bookMarkDate: req.body.bookMarkDate,
-        bookPages: req.body.bookPages,
-        readStartDate: req.body.readStartDate,
-    }, { new: true })
-        .then(book => {
-            if (!book) {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            res.send(book);
-        }).catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            return res.status(500).send({
-                message: "Error updating book with id " + req.params.bookId
-            });
-        });
-};
+export function update(req: Request, res: Response): void {
+  // Validate Request
+  if (!req.body.bookName || !req.body.bookAuthor || !req.body.bookPages || !req.body.bookMarkDate
+    || !req.body.bookPages || !req.body.readStartDate) {
+    res.status(400).send({
+      message: 'needed book info can not be empty',
+    });
+  }
 
-export function remove(req: Request, res: Response) {
-    Book.findByIdAndRemove(req.params.bookId)
-        .then(book => {
-            if (!book) {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            res.send({ message: "book deleted successfully!" });
-        }).catch(err => {
-            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            return res.status(500).send({
-                message: "Could not delete book with id " + req.params.bookId
-            });
+  Book.findByIdAndUpdate(req.params.bookId, {
+    bookName: req.body.bookName,
+    bookAuthor: req.body.bookAuthor,
+    bookImg: req.body.bookImg,
+    bookMark: req.body.bookMark,
+    bookMarkDate: req.body.bookMarkDate,
+    bookPages: req.body.bookPages,
+    readStartDate: req.body.readStartDate,
+  }, { new: true })
+    .then((book) => {
+      if (!book) {
+        res.status(404).send({
+          message: `book not found with id ${req.params.bookId}`,
         });
-};
+      }
+      res.send(book);
+    }).catch((err) => {
+      if (err.kind === 'ObjectId') {
+        res.status(404).send({
+          message: `book not found with id ${req.params.bookId}`,
+        });
+      }
+      res.status(500).send({
+        message: `Error updating book with id ${req.params.bookId}`,
+      });
+    });
+}
+
+export function remove(req: Request, res: Response): void {
+  Book.findByIdAndRemove(req.params.bookId)
+    .then((book) => {
+      if (!book) {
+        res.status(404).send({
+          message: `book not found with id ${req.params.bookId}`,
+        });
+      }
+      res.send({ message: 'book deleted successfully!' });
+    }).catch((err) => {
+      if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+        res.status(404).send({
+          message: `book not found with id ${req.params.bookId}`,
+        });
+      }
+      res.status(500).send({
+        message: `Could not delete book with id ${req.params.bookId}`,
+      });
+    });
+}

7dfaba5aa63ae36bb9a7f713299d5e4e39ccce3b
Matti Määttänen <matti.maatt@gmail.com>
2021-03-20 01:59:20 +0200

    added error catch for mongoose connect, linting for index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,32 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import router from './routes/routes';
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-const port = process.env.REACT_APP_BACKPORT || 8080
-const DB_URL = process.env.MONGO_URL || "mongodb://localhost:27018/books"
+const port = process.env.REACT_APP_BACKPORT || 8080;
+const DB_URL = process.env.MONGO_URL || 'mongodb://localhost:27018/books';
 
-console.log(DB_URL)
-app.use(express.urlencoded({ extended: true }))
-
-app.use(express.json())
-
-const mongoose = require('mongoose');
+// TODO: better logging config
+console.log(DB_URL); // eslint-disable-line no-console
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(router);
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect(DB_URL, {
-
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 }).then(() => {
-    console.log("Successfully connected to the database", DB_URL);
+  console.info('Successfully connected to the database', DB_URL); // eslint-disable-line no-console
+}).catch((err) => {
+  console.error(err); // eslint-disable-line no-console
 });
 
-
-app.use(router)
-
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
-})
+  console.info(`Server listening at http://localhost:${port}`); // eslint-disable-line no-console
+});

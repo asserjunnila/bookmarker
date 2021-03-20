b101e3bc2bc18ad6036b7c2a85f74fb06d7e5197
Matti Määttänen <matti.maatt@gmail.com>
2021-03-20 02:14:26 +0200

    fixed ident

--- backend/src/controllers/book.controller.ts
+++ backend/src/controllers/book.controller.ts
@@ -1,101 +1,98 @@
-import Book from '../models/book.model';
 import { Request, Response } from 'express';
+import Book from '../models/book.model';
 
 export function create(req: Request, res: Response) {
-    const { bookName, bookAuthor, bookImg, bookMark, bookMarkDate, bookPages, readStartDate, bookISBN } = req.body;
-    const book = Book.build({ bookName, bookAuthor, bookImg, bookMark, bookMarkDate, bookPages, readStartDate, bookISBN });
-    book.save()
-
+  const { bookName, bookAuthor, bookImg, bookMark, bookMarkDate, bookPages, readStartDate, bookISBN } = req.body;
+  const book = Book.build({ bookName, bookAuthor, bookImg, bookMark, bookMarkDate, bookPages, readStartDate, bookISBN });
+  book.save();
 }
 
 export function findAll(req: Request, res: Response) {
-
-    const book = Book.find({})
-        .then(books => {
-            res.send({ "books": books });
-        }).catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving books."
-            });
-        });
-
-};
+  const book = Book.find({})
+    .then((books) => {
+      res.send({ books: books });
+    }).catch((err) => {
+      res.status(500).send({
+        message: err.message
+      });
+    });
+}
 
 export function findOne(req: Request, res: Response) {
-    Book.findById(req.params.bookId)
-        .then(book => {
-            if (!book) {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            res.send(book);
-        }).catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            return res.status(500).send({
-                message: "Error retrieving book with id " + req.params.bookId
-            });
+  Book.findById(req.params.bookId)
+    .then(book => {
+      if (!book) {
+        return res.status(404).send({
+          message: 'book not found with id ${req.params.bookId}',
         });
-};
+      }
+      res.send(book);
+    }).catch(err => {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).send({
+          message: 'book not found with id ${req.params.bookId}',
+        });
+      }
+      return res.status(500).send({
+        message: 'Error retrieving book with id ' + req.params.bookId
+      });
+    });
+}
 
 export function update(req: Request, res: Response) {
 
-    if (!req.body.bookName || !req.body.bookAuthor || !req.body.bookPages || !req.body.bookMarkDate || !req.body.bookPages || !req.body.readStartDate) {
-        return res.status(400).send({
-            message: `needed book info can not be empty`
-        });
-    }
+  if (!req.body.bookName || !req.body.bookAuthor || !req.body.bookPages || !req.body.bookMarkDate || !req.body.bookPages || !req.body.readStartDate) {
+    return res.status(400).send({
+      message: `needed book info can not be empty`
+    });
+  }
 
-    Book.findByIdAndUpdate(req.params.bookId, {
-        bookName: req.body.bookName,
-        bookAuthor: req.body.bookAuthor,
-        bookImg: req.body.bookImg,
-        bookMark: req.body.bookMark,
-        bookMarkDate: req.body.bookMarkDate,
-        bookPages: req.body.bookPages,
-        readStartDate: req.body.readStartDate,
-        bookISBN: req.body.bookISBN,
-    }, { new: true })
-        .then(book => {
-            if (!book) {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            res.send(book);
-        }).catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            return res.status(500).send({
-                message: "Error updating book with id " + req.params.bookId
-            });
+  Book.findByIdAndUpdate(req.params.bookId, {
+    bookName: req.body.bookName,
+    bookAuthor: req.body.bookAuthor,
+    bookImg: req.body.bookImg,
+    bookMark: req.body.bookMark,
+    bookMarkDate: req.body.bookMarkDate,
+    bookPages: req.body.bookPages,
+    readStartDate: req.body.readStartDate,
+    bookISBN: req.body.bookISBN,
+  }, { new: true })
+    .then(book => {
+      if (!book) {
+        return res.status(404).send({
+          message: "book not found with id " + req.params.bookId
         });
-};
+      }
+      res.send(book);
+    }).catch(err => {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).send({
+          message: "book not found with id " + req.params.bookId
+        });
+      }
+      return res.status(500).send({
+        message: "Error updating book with id " + req.params.bookId
+      });
+    });
+}
 
 export function deleteBook(req: Request, res: Response) {
-    Book.findByIdAndRemove(req.params.bookId)
-        .then(book => {
-            if (!book) {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            res.send({ message: "book deleted successfully!" });
-        }).catch(err => {
-            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
-                return res.status(404).send({
-                    message: "book not found with id " + req.params.bookId
-                });
-            }
-            return res.status(500).send({
-                message: "Could not delete book with id " + req.params.bookId
-            });
+  Book.findByIdAndRemove(req.params.bookId)
+    .then(book => {
+      if (!book) {
+        return res.status(404).send({
+          message: "book not found with id " + req.params.bookId
         });
-};
+      }
+      res.send({ message: "book deleted successfully!" });
+    }).catch(err => {
+      if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+        return res.status(404).send({
+          message: "book not found with id " + req.params.bookId
+        });
+      }
+      return res.status(500).send({
+        message: "Could not delete book with id " + req.params.bookId
+      });
+    });
+}
--- backend/src/models/book.model.ts
+++ backend/src/models/book.model.ts
@@ -1,73 +1,73 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 interface IBook {
-    bookName: string;
-    bookAuthor: string;
-    bookImg: string;
-    bookMark: number;
-    bookMarkDate: Date;
-    bookPages: number;
-    readStartDate: Date;
-    bookISBN: number;
+  bookName: string;
+  bookAuthor: string;
+  bookImg: string;
+  bookMark: number;
+  bookMarkDate: Date;
+  bookPages: number;
+  readStartDate: Date;
+  bookISBN: number;
 }
 
-interface bookModelInterface extends mongoose.Model<BookDoc> {
-    build(attr: IBook): BookDoc
+interface BookModelInterface extends mongoose.Model<BookDoc> {
+  build(attr: IBook): BookDoc
 }
 
 interface BookDoc extends mongoose.Document {
-    bookName: string;
-    bookAuthor: string;
-    bookImg: string;
-    bookMark: number;
-    bookMarkDate: Date;
-    bookPages: number;
-    readStartDate: Date;
-    bookISBN: number;
+  bookName: string;
+  bookAuthor: string;
+  bookImg: string;
+  bookMark: number;
+  bookMarkDate: Date;
+  bookPages: number;
+  readStartDate: Date;
+  bookISBN: number;
 }
 
 const bookSchema = new mongoose.Schema({
-    bookName: {
-        type: String,
-        required: true
-    },
-    bookAuthor: {
-        type: String,
-        required: true
-    },
-    bookImg: {
-        type: String,
-        required: true
-    },
-    bookMark: {
-        type: Number,
-        required: true
-    },
-    bookMarkDate: {
-        type: Date,
-        required: true
-    },
-    bookPages: {
-        type: Number,
-        required: true
-    },
-    readStartDate: {
-        type: Date,
-        required: true
-    },
-    bookISBN: {
-        type: Number,
-        required: false
-    },
+  bookName: {
+    type: String,
+    required: true,
+  },
+  bookAuthor: {
+    type: String,
+    required: true,
+  },
+  bookImg: {
+    type: String,
+    required: true,
+  },
+  bookMark: {
+    type: Number,
+    required: true,
+  },
+  bookMarkDate: {
+    type: Date,
+    required: true,
+  },
+  bookPages: {
+    type: Number,
+    required: true,
+  },
+  readStartDate: {
+    type: Date,
+    required: true,
+  },
+  bookISBN: {
+    type: Number,
+    required: false,
+  },
 
 }, {
-    timestamps: true
+  timestamps: true,
 });
 
 bookSchema.statics.build = (attr: IBook) => {
-    return new Book(attr)
-}
+  return new Book(attr);
+};
 
-const Book = mongoose.model<BookDoc, bookModelInterface>('Book', bookSchema)
+const Book = mongoose.model<BookDoc, BookModelInterface>('Book', bookSchema);
 
-export default Book
+export default Book;
